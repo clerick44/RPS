161870f62832fe4cf74f8477cce3c7ca27cecf0e
clerick44 <clerick44@hotmail.com>
2021-03-02 19:57:22 -0600

    computerPlay function added

---
+++ script.js
@@ -0,0 +1,9 @@
+function computerPlay() {
+    let compTurnResult = ""
+    let compTurn = math.floor(math.random() *3)
+    if compTurn = 1 {compTurnResult = "Rock"
+                    return compTurnResult}
+    else if compTurn = 2 [compTurnResult = "Paper"]
+    else { compTurnResult = "Scissors"}
+console.log 
+}
